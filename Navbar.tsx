174f496e6531e997cb6c2416400bc05499cc1934
eitextt-code <eitextt@gmail.com>
2026-07-18 14:43:23 +0200

    Create Navbar.tsx

---
+++ Navbar.tsx
@@ -0,0 +1,37 @@
+export default function Navbar(){
+
+return(
+
+<nav className="fixed w-full bg-[#F7F3EB]/90 backdrop-blur-md z-50">
+
+<div className="container flex justify-between py-5">
+
+<h2>Café Casa Quintano</h2>
+
+<ul className="flex gap-8">
+
+<li>Inicio</li>
+
+<li>Nuestra Cocina</li>
+
+<li>Carta</li>
+
+<li>Especialidades</li>
+
+<li>Galería</li>
+
+<li>Opiniones</li>
+
+<li>Reservas</li>
+
+<li>Contacto</li>
+
+</ul>
+
+</div>
+
+</nav>
+
+)
+
+}
