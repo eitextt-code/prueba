450cdf84deebae4ad6b619aaa1dcc9a1f2d3378d
eitextt-code <eitextt@gmail.com>
2026-07-18 14:44:11 +0200

    Create Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,31 @@
+export default function Footer(){
+
+return(
+
+<footer className="bg-[#4A3728] text-white p-10 text-center">
+
+<p>Café Casa Quintano</p>
+
+<p>
+
+Calle Consistorio · Casaseca de Campeán
+
+</p>
+
+<p>
+
+980 56 09 53
+
+</p>
+
+<p>
+
+cafedequintano@gmail.com
+
+</p>
+
+</footer>
+
+)
+
+}
