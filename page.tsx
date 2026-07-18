92ed613193f3750fa70308d45eab5334e2910ecd
eitextt-code <eitextt@gmail.com>
2026-07-18 14:42:57 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+export default function Home(){
+
+return(
+
+<>
+
+<Navbar/>
+
+<Hero/>
+
+<Footer/>
+
+</>
+
+)
+
+}
